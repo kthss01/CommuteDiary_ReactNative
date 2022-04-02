6b3aabf50aab909106151ab3737e0d6e58ff7b73
kthss01 <servant0416@gmail.com>
2022-04-02 17:10:14 +0900

    시작 종료 버튼 추가, 시간 경과 문제 이슈있음

--- practice/timer_ex2/src/TimerComponent.js
+++ practice/timer_ex2/src/TimerComponent.js
@@ -1,27 +1,46 @@
 import React, {useEffect, useState, useRef} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import useInterval from './useInterval';
+
+const TIME_DELAY = 1;
+const DELAY_DIFF = 1000 / TIME_DELAY;
 
 export default function TimerComponent() {
   const [hour, setHour] = useState(0);
   const [min, setMin] = useState(0);
   const [sec, setSec] = useState(0);
   const [ms, setMs] = useState(0);
+  const [running, setRunning] = useState(false);
   const time = useRef(0);
 
-  useInterval(() => {
-    setMs(time.current % 100);
-    setSec(parseInt(time.current / 1000) % 60);
-    setMin(parseInt(time.current / (1000 * 60)) % 60);
-    setHour(parseInt(time.current / (1000 * 60 * 60)) % 24);
-    time.current += 1;
-  }, 1);
+  useInterval(
+    () => {
+      setMs(time.current % DELAY_DIFF);
+      setSec(parseInt(time.current / DELAY_DIFF) % 60);
+      setMin(parseInt(time.current / (DELAY_DIFF * 60)) % 60);
+      setHour(parseInt(time.current / (DELAY_DIFF * 60 * 60)) % 24);
+      time.current += 1;
+    },
+    running ? TIME_DELAY : null,
+  );
 
   return (
     <View>
       <Text>
         {hour}:{min}:{sec}:{ms}
       </Text>
+      <Button
+        title="시작"
+        onPress={() => {
+          setRunning(true);
+        }}
+      />
+      <Button
+        title="종료"
+        onPress={() => {
+          setRunning(false);
+        }}
+      />
     </View>
   );
 }
